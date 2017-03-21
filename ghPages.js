dd745d6972f1acec1896e447e8a9fdea3438eb20
Per-Erik Karlsson <per-erik.karlsson@tieto.com>
2017-03-21 14:57:14 +0100

    Added build folder.

--- ghPages.js
+++
@@ -1,8 +0,0 @@
-var ghpages = require('gh-pages');
-var path = require('path');
-
-var options = {
-  push: false
-};
-
-ghpages.publish(path.join(__dirname, 'build'), options, function(err) { console.log('err', err) });
