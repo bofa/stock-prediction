826b121a632a979013d1b1a12e7f5b91574b0940
bofa <prosit86@gmail.com>
2017-03-23 22:43:20 +0100

    Woot

--- src/js/routes.js
+++ src/js/routes.js
@@ -6,51 +6,49 @@ import StockApp from './containers/StockApp/StockApp';
 import Company from './views/Company';
 import NotFoundView from './views/NotFoundView';
 
-const gitHubRepoName = 'stock-prediction';
-// The domain for your site
-// SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
-const domain = 'https://bofa.github.io';
-function redirectToDomain() {
-  window.location.replace(domain);
-}
+// const gitHubRepoName = 'stock-prediction';
+// // The domain for your site
+// // SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
+// const domain = 'https://bofa.github.io';
+// function redirectToDomain() {
+//   window.location.replace(domain);
+// }
 
-function parseRedirectQuery(query, replace) {
-  let redirectTo = {};
+// function parseRedirectQuery(query, replace) {
+//   let redirectTo = {};
 
-  if (typeof query.pathname === 'string' && query.pathname !== '') {
-    redirectTo.pathname = query.pathname;
-  }
+//   if (typeof query.pathname === 'string' && query.pathname !== '') {
+//     redirectTo.pathname = query.pathname;
+//   }
 
-  if (typeof query.query === 'string' && query.query !== '') {
-    let queryObject = {};
-    query.query.split('&').map(q => q.split('=')).forEach(arr => {
-      queryObject[arr[0]] = arr.slice(1).join('=');
-    })
-    redirectTo.query = queryObject;
-  }
+//   if (typeof query.query === 'string' && query.query !== '') {
+//     let queryObject = {};
+//     query.query.split('&').map(q => q.split('=')).forEach(arr => {
+//       queryObject[arr[0]] = arr.slice(1).join('=');
+//     })
+//     redirectTo.query = queryObject;
+//   }
 
-  if (typeof query.hash === 'string' && query.hash !== '') {
-    redirectTo.hash = `#${query.hash}`;
-  }
+//   if (typeof query.hash === 'string' && query.hash !== '') {
+//     redirectTo.hash = `#${query.hash}`;
+//   }
 
-  replace(redirectTo);
-}
+//   replace(redirectTo);
+// }
 
-function checkForRedirect(nextState, replace) {
-  const location = nextState.location;
-  if (location.query.redirect === 'true') {
-    parseRedirectQuery(location.query, replace);
-  } else if (location.pathname.split('/')[1] === gitHubRepoName) {
-    redirectToDomain();
-  }
-}
+// function checkForRedirect(nextState, replace) {
+//   const location = nextState.location;
+//   if (location.query.redirect === 'true') {
+//     parseRedirectQuery(location.query, replace);
+//   } else if (location.pathname.split('/')[1] === gitHubRepoName) {
+//     redirectToDomain();
+//   }
+// }
 
 export default (
   <Route path="/stock-prediction" component={App} /*onEnter={checkForRedirect}*/>
     <IndexRoute component={StockApp} />
-    <Route path="company/:company" component={Company} >
-
-    </Route>
+    <Route path="company/:company" component={Company} />
     <Route path="404" component={NotFoundView} />
     <Redirect from="*" to="404" />
   </Route>
