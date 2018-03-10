d54a5145dd130b83531b0915198a69a9daabc9c5
bofa <prosit86@gmail.com>
2018-03-10 20:03:03 +0100

    new build

--- src/js/data/index.js
+++
@@ -1,6 +0,0 @@
-import earningsData from './earnings.json';
-import { fromJS, Map } from 'immutable';
-import manualData from './manualData';
-
-export default Map(earningsData.map(v => [v.ShortName, fromJS(v)]))
-  .mergeDeep(manualData);
--- src/js/services/statistics.js
+++
@@ -1,7 +0,0 @@
-import { Iterable } from 'immutable';
-
-export function average(list) {
-  const length = Iterable.isIterable(list) ? list.size : list.length;
-
-  return list.reduce((out, sum) => out + sum, 0) / length;
-}
